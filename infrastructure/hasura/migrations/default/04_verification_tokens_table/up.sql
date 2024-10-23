8616e80d0b3943ab8d2bbed00ae1e6a89b7ddc94
Chris Sprance <chrissprance@gmail.com>
2024-10-23 15:25:27 -0400

    get rid of some account stuff to keep things simple not every project needs auth and there are better default out there

--- infrastructure/hasura/migrations/default/04_verification_tokens_table/up.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE public.verification_tokens (
-    token text NOT NULL,
-    identifier text NOT NULL,
-    expires timestamptz
-);
-
-ALTER TABLE ONLY public.verification_tokens
-    ADD CONSTRAINT verification_tokens_pkey PRIMARY KEY (token);
